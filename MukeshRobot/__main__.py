efa1fafdf3bfaf52de0fdcd9e200c79ba162583a
Anonymous <adi855053@gmail.com>
2023-07-24 20:27:10 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -238,7 +238,7 @@ def start(update: Update, context: CallbackContext):
             time.sleep(0.6)
             lol.delete()
             
-            update.effective_message.reply_text(PM_START_TEXT.format(escape_markdown(first_name), PM_IMG, BOT_NAME,sql.num_users(),sql.num_chats(),START_IMG),
+            update.effective_message.reply_text(PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME, PM_IMG, sql.num_users(), sql.num_chats(), START_IMG),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,

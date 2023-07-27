ba35d0ebdf3910d8ec2481676949584e3f3a2acb
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2023-07-27 20:47:19 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -102,8 +102,9 @@ buttons = [
      [   
         InlineKeyboardButton(text="🤝𝕆𝕨𝕟𝕖𝕣🤝", url=f"tg://user?id={OWNER_ID}"),
      ],  
+    [
         InlineKeyboardButton(text="😝ℂ𝕙𝕒𝕜𝕣𝕒😝", callback_data="Main_help"),
-     ],
+     
     
         InlineKeyboardButton(
             text="😍𝕾𝖚𝖒𝖒𝖔𝖓 𝕸𝖊😍",

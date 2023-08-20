268e99073e227b0596b178baea441676f3522a32
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2023-08-20 09:53:15 +0530

    Update aifunctions.py

--- MukeshRobot/modules/aifunctions.py
+++ MukeshRobot/modules/aifunctions.py
@@ -28,7 +28,7 @@ async def hmm(event):
     lodu = response.json()
     result = lodu["message"]["text"]
     if "no no" in result:
-        pro = "I am fairly found and I was made by @itz_legend_coder."
+        pro = "I am fairly found and I was made by @speedy208."
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
@@ -36,7 +36,7 @@ async def hmm(event):
         except CFError as e:
             print(e)
     elif "ann" in result:
-        pro = "My name is Mukesh"
+        pro = "My name is Kakashi"
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)

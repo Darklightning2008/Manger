60ebcb2414ef5df6c6d31423bac3f08a5db2ae21
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2023-08-21 21:14:46 +0530

    Update broadcast.py

--- MukeshRobot/modules/broadcast.py
+++ MukeshRobot/modules/broadcast.py
@@ -1,82 +1,43 @@
 import asyncio
 
-from pyrogram import Client, filters
+from pyrogram import filters
+from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-import MukeshRobot.modules.sql.users_sql as sql
-from MukeshRobot import DEV_USERS, OWNER_ID, pbot as pgram
-from MukeshRobot.modules.sql.users_sql import get_all_users
+from config import OWNER_ID
+from MukeshRobot import app, app2
 
-#Broadcast Function
-@pgram.on_message(filters.command("broadcast"))
-async def broadcast_cmd(client: Client, message: Message):
-    user_id = message.from_user.id
-    texttt = message.text.split(" ")
 
-    if user_id not in [OWNER_ID] + DEV_USERS:
-        await message.reply_text(
-            "You are not authorized to use this command. Only the owner and authorized users can use it."
-        )
-        return
-
-    if len(texttt) < 2:
-        return await message.reply_text(
-            "<b>BROADCASTING COMMANDS</b>\n-user : broadcasting all user's DM\n-group : broadcasting all groups\n-all : broadcasting both\nEx: /broadcast-user"
-        )
-
-    if message.reply_to_message is None and not get_arg(message):
-        return await message.reply_text(
-            "<b>Please provide a message or reply to a message</b>"
-        )
-
-    tex = await message.reply_text("<code>Starting global broadcast...</code>")
-
-    usersss = 0
-    chatttt = 0
-    uerror = 0
-    cerror = 0
-    chats = sql.get_all_chats() or []
-    users = get_all_users()
-
-    if "-all" in texttt:
-        texttt.append("-user")
-        texttt.append("-group")
-
-    if "-user" in texttt:
-        for chat in users:
-            if message.reply_to_message:
-                msg = message.reply_to_message
-            else:
-                msg = get_arg(message)
-            try:
-                if message.reply_to_message:
-                    aa = await msg.copy(chat.user_id)
-                else:
-                    aa = await client.send_message(chat.user_id, msg)
-
-                usersss += 1
-                await asyncio.sleep(0.3)
-            except Exception:
-                uerror += 1
-                await asyncio.sleep(0.3)
-    if "-group" in texttt:
-        for chat in chats:
-            if message.reply_to_message:
-                msg = message.reply_to_message
-            else:
-                msg = get_arg(message)
-            try:
-                if message.reply_to_message:
-                    aa = await msg.copy(chat.chat_id)
-                else:
-                    aa = await client.send_message(chat.chat_id, msg)
-
-                chatttt += 1
-                await asyncio.sleep(0.3)
-            except Exception:
-                cerror += 1
-                await asyncio.sleep(0.3)
-
-    await tex.edit_text(
-        f"<b>Message Successfully Sent</b> \nTotal Users: <code>{usersss}</code> \nFailed Users: <code>{uerror}</code> \nTotal GroupChats: <code>{chatttt}</code> \nFailed GroupChats: <code>{cerror}</code>"
-    )
+@app.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
+async def broadcast(_, message: Message):
+    brep = await message.reply_text("sᴛᴀʀᴛᴇᴅ ᴀssɪsᴛᴀɴᴛ ʙʀᴏᴀᴅᴄᴀsᴛ...")
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text(
+                "**ᴇxᴀᴍᴘʟᴇ:**\n\n/broadcast [ᴍᴇssᴀɢᴇ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ]"
+            )
+        query = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    async for dialog in app2.get_dialogs():
+        chats.append(int(dialog.chat.id))
+    for i in chats:
+        try:
+            await app2.forward_messages(
+                i, y, x
+            ) if message.reply_to_message else await app2.send_message(i, text=query)
+            sent += 1
+        except FloodWait as e:
+            flood_time = int(e.value)
+            if flood_time > 200:
+                continue
+            await asyncio.sleep(flood_time)
+        except Exception:
+            continue
+    try:
+        await brep.edit_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
+    except:
+        await message.reply_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ {sent} ᴄʜᴀᴛs.**")
